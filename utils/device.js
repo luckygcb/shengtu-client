4f610cda9159fa4ebfd6e690998d5a3c96971673
迟善琳 <chishanlin@kanyun.com>
2024-10-14 17:56:16 +0800

    refactor: deviceId

--- utils/device.js
+++ utils/device.js
@@ -1,19 +1,15 @@
-import  { Platform } from 'react-native';
 import uuid from 'react-native-uuid';
-import DeviceInfo from 'react-native-device-info';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export async function getDeviceId () {
-  if (Platform.OS === 'web') {
-    let deviceId = localStorage.getItem('echo_journey_deviceId');
-    if (deviceId) {
-      return deviceId;
-    }
 
-    deviceId = uuid.v4();
-    localStorage.setItem('echo_journey_deviceId', deviceId);
-
+  let deviceId = await AsyncStorage.getItem('echo_journey_deviceId');
+  if (deviceId) {
     return deviceId;
   }
 
-  return DeviceInfo.getUniqueId();
+  deviceId = uuid.v4();
+  await AsyncStorage.setItem('echo_journey_deviceId', deviceId);
+
+  return deviceId;
 }
